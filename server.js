b4eb1fe3bbe235de4893db6e517ced9037393fa5
Your Name <migs432@yahoo.com>
2016-10-10 21:21:31 -0700

    very simple express server app

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var connect = require('connect');
-var app = require('connect')();
+var express = require('express');
+var app = express();
+
 
 var htmldoc =  "<CENTER><h1>HELLO WORLD</h1>"
              +"<p>Simple request and response page</p></CENTER>"
 
-var helloWorld = function(req, res, next) {
-                                          res.setHeader('Content-Type', 'text/html');
-                                          res.end(htmldoc);
-                                        };
-app.use(helloWorld);
+app.use('/', function(req, res) {
+                                  res.send(htmldoc);
+                                }
+       );
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
 
+module.exports = app;
