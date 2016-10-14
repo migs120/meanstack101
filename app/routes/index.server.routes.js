944c87e95d96a76d36f722757c97f5d38f55179e
Your Name <migs432@yahoo.com>
2016-10-14 10:46:30 -0700

    testing arguments on required function

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,4 +1,6 @@
-module.exports = function(app) {
-                                var index = require('../controllers/index.server.controller');
-                                app.get('/', index.render);
-                                };
+module.exports = function(app, argmt2) {
+                                        console.log(argmt2) //testing
+                                        var index = require('../controllers/index.server.controller');
+                                        app.get('/', index.render);
+
+                                        };
--- config/express.js
+++ config/express.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 module.exports = function() {
                               var app = express();
-                              require('../app/routes/index.server.routes.js')(app);
+                         
+                             require('../app/routes/index.server.routes.js')(app,'testArgmt')  //arguments get passed to routes.js
+                          
                               return app;
                             };
