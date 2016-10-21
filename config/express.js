7fca30723bfe4f122e767e6259b19cab93d4bdca
Your Name <migs432@yahoo.com>
2016-10-21 11:50:15 -0700

    added the first model user to app

--- config/express.js
+++ config/express.js
@@ -49,6 +49,7 @@ module.exports = function() {
     
                          
                              require('../app/routes/index.server.routes.js')(app,'testArgmt')  //arguments get passed to routes.js
+                             require('../app/routes/users.server.routes.js')(app);
                              
                              app.use(express.static('./public'));
                           
