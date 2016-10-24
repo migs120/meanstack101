8599dc02434e52ff5aec079026c92dbd0e890bf1
Your Name <migs432@yahoo.com>
2016-10-24 12:37:48 -0700

    added examine example for res.send

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,6 +1,12 @@
 module.exports = function(app, argmt2) {
+    
+                                        var htmldoc =  "<CENTER><h1>HELLO WORLD</h1>"
+                                                      +"<p>Simple request and response page</p></CENTER>"
+                                        
                                         console.log(argmt2, "-- consoled at index.server.routes.js") //testing
+                                        
                                         var index = require('../controllers/index.server.controller');
+    
                                         app.get('/', index.render);
     
                                         app.get('/mig', function(req, res, next) {
@@ -10,6 +16,11 @@ module.exports = function(app, argmt2) {
                                                                                                             }
                                                                                                 )
                                                                                  }
+                                               );
+    
+                                        app.get('/migtwo', function(req, res, next) {
+                                                                                       res.send(htmldoc);
+                                                                                     }
                                                );
 
                                         };
