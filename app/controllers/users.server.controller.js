6b85c94b8ab2e82a8925be67e454946f5a780fe7
Your Name <migs432@yahoo.com>
2016-10-25 02:06:35 -0700

    added delete logic to user and route

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -67,6 +67,18 @@ exports.update = function(req, res, next) {
                                             };
 
 
+exports.delete = function(req, res, next) {
+                                            req.user.remove(
+                                                             function(err) {
+                                                                            if (err) {
+                                                                                       return next(err);
+                                                                                     }
+                                                                            else {
+                                                                                   res.json(req.user);
+                                                                                 }
+                                                                          }
+                                                           )
+                                          };
 
 
 
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -7,7 +7,9 @@ module.exports = function(app) {
     
                                   app.route('/users/:userId')
                                       .get(users.read)
-                                      .put(users.update);
+                                      .put(users.update)
+                                      .delete(users.delete);
+
 
                                   app.param('userId', users.userByID);
 
