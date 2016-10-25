97037977244c79e8b0dfa82bf57d3515187d5ca2
Your Name <migs432@yahoo.com>
2016-10-25 02:32:45 -0700

    added a custom getter modefier to user schema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,13 +5,34 @@ var UserSchema = new Schema({
                               firstName: String,
                               lastName: String,
                               email: String,
-                              username: String,
+                              username: {
+                                          type: String,
+                                          trim: true
+                                        },
                               password: String,
                               created: {
                                         type: Date,
                                         default: Date.now
-                                       }
+                                       },
+                              website: {
+                                        type: String,
+                                        get: function(url) {
+                                                            if (!url) {
+                                                                        return url;
+                                                                      } 
+                                                             else {
+                                                                    if (url.indexOf('http://') !== 0   && url.indexOf('https://') !== 0) {
+                                                                                                                                           url = 'http://' + url;
+                                                                                                                                         }
+
+                                                                    return url;
+                                                                   }
+                                                            }
+                                        }
+
 
                             });
+
+UserSchema.set('toJSON', { getters: true });
 
 mongoose.model('User', UserSchema);
