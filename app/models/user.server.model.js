c74b9b26c9ff1103874f93c3bd6e58993e4a17e6
Your Name <migs432@yahoo.com>
2016-10-27 12:19:37 -0700

    added schema validations

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -6,15 +6,31 @@ var UserSchema = new Schema({
                               lastName: String,
                               email: {
                                         type: String,
-                                        index: true
+                                        index: true,
+                                        match: /.+\@.+\..+/
+
                                       },
                               username: {
                                           type: String,
                                           trim: true,
-                                          unique: true
+                                          unique: true,
+                                          required: true
 
                                         },
-                              password: String,
+                              password: {
+                                          type: String,
+                                          validate: [
+                                                      function(password) {
+                                                                            return password.length >= 6;
+                                                                          },
+                                                      'Password should be longer'
+                                                    ]
+                                        },
+                              role: {
+                                      type: String,
+                                      enum: ['Admin', 'Owner', 'User']
+                                     },
+
                               created: {
                                         type: Date,
                                         default: Date.now
