5b5f3ccd6599ee49b933643ea512db7b3ea6fd90
Your Name <migs432@yahoo.com>
2016-12-30 10:33:19 -0800

    added angular controller to example view

---
+++ public/example/controllers/example.client.controller.js
@@ -0,0 +1,9 @@
+angular.module('example').controller(
+                                     'ExampleController'
+                                     , [
+                                        '$scope'
+                                        ,function($scope) {
+                                                            $scope.name = 'MEAN Application';
+                                                          }
+                                        ]
+                                    );
