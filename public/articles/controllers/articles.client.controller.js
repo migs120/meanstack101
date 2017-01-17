ec66711090d41fb47a7f939ee9ff5cf5f8f9bd2d
Your Name <migs432@yahoo.com>
2017-01-17 06:06:23 -0800

    setting up angular js module

---
+++ public/articles/controllers/articles.client.controller.js
@@ -0,0 +1,13 @@
+angular.module('articles').controller(
+    
+    
+    
+    'ArticlesController'
+    , ['$scope', '$routeParams', '$location', 'Authentication', 'Articles',
+          function($scope, $routeParams, $location, Authentication, Articles) {
+                                                                                $scope.authentication = Authentication;
+                                                                              }
+      ]
+
+
+                                    );
